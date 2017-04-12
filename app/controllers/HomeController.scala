d5bc5250f4bbd76b118b70ea4f701cb1b48ef479
Johann-Michael Thiebaut <johann-michael.thiebaut@epfl.ch>
2017-04-12 11:58:25 +0200

    Adding minimal play app files

---
+++ app/controllers/HomeController.scala
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2017 - Swiss Data Science Center (SDSC)
+ * A partnership between École Polytechnique Fédérale de Lausanne (EPFL) and
+ * Eidgenössische Technische Hochschule Zürich (ETHZ).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import javax.inject._
+
+import play.api.mvc._
+
+/**
+  * This controller creates an `Action` to handle HTTP requests to the
+  * application's home page.
+  */
+@Singleton
+class HomeController @Inject() extends Controller {
+
+  def index = Action { implicit request =>
+    Ok
+  }
+
+}
