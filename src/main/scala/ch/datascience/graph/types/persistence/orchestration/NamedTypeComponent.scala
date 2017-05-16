578b89c17c25a3cd331f69817fb850aa68f737e9
Johann-Michael Thiebaut <johann-michael.thiebaut@epfl.ch>
2017-05-16 14:15:39 +0200

    Refactor on NamedType

--- src/main/scala/ch/datascience/graph/types/persistence/orchestration/NamedTypeComponent.scala
+++ src/main/scala/ch/datascience/graph/types/persistence/orchestration/NamedTypeComponent.scala
@@ -21,7 +21,7 @@ package ch.datascience.graph.types.persistence.orchestration
 import java.util.UUID
 
 import ch.datascience.graph.NamespaceAndName
-import ch.datascience.graph.types.persistence.model.{GraphDomain, NamedType}
+import ch.datascience.graph.types.persistence.model.{GraphDomain, NamedType, PropertyKey}
 import ch.datascience.graph.types.persistence.model.relational.{RowNamedType, RowPropertyKey}
 
 import language.{higherKinds, reflectiveCalls}
@@ -62,7 +62,8 @@ trait NamedTypeComponent {
       graphDomain: GraphDomain,
       name: String,
       superTypesMap: Map[NamespaceAndName, RowNamedType],
-      propertiesMap: Map[NamespaceAndName, RowPropertyKey]
+//      propertiesMap: Map[NamespaceAndName, RowPropertyKey]
+      propertiesMap: Map[NamespaceAndName, PropertyKey]
     ): Future[NamedType] = {
       val namedType = NamedType(UUID.randomUUID(), graphDomain, name, superTypesMap, propertiesMap)
       val insertNamedType = dal.namedTypes add namedType
@@ -86,7 +87,8 @@ trait NamedTypeComponent {
       joinedFuture flatMap { case (graphDomain, superTypesMap, propertiesMap) => createNamedType(graphDomain, name, superTypesMap, propertiesMap) }
     }
 
-    def linearize(superTypes: Set[NamespaceAndName], properties: Set[NamespaceAndName]): Future[(Map[NamespaceAndName, RowNamedType], Map[NamespaceAndName, RowPropertyKey])] = {
+//    def linearize(superTypes: Set[NamespaceAndName], properties: Set[NamespaceAndName]): Future[(Map[NamespaceAndName, RowNamedType], Map[NamespaceAndName, RowPropertyKey])] = {
+    def linearize(superTypes: Set[NamespaceAndName], properties: Set[NamespaceAndName]): Future[(Map[NamespaceAndName, RowNamedType], Map[NamespaceAndName, PropertyKey])] = {
       val futureSuperTypes = for {
         iterable <- Future.traverse(superTypes.toIterable)(this.findByNamespaceAndName)
       } yield for {
@@ -117,7 +119,7 @@ trait NamedTypeComponent {
         superTypes <- futureSuperTypes
         directProperties = for {
           property <- properties
-        } yield property.key -> property.toRow
+        } yield property.key -> property
         impliedProperties = for {
           superType <- superTypes
           (key, value) <- superType.propertiesMap
