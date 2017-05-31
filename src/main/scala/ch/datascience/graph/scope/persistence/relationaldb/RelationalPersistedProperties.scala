3d070135c7f1a89d4ab6fc9287aa32791370729c
Johann-Michael Thiebaut <johann-michael.thiebaut@epfl.ch>
2017-05-31 14:24:15 +0200

    Changes following de-genericization

--- src/main/scala/ch/datascience/graph/scope/persistence/relationaldb/RelationalPersistedProperties.scala
+++ src/main/scala/ch/datascience/graph/scope/persistence/relationaldb/RelationalPersistedProperties.scala
@@ -20,14 +20,14 @@ package ch.datascience.graph.scope.persistence.relationaldb
 
 import ch.datascience.graph.naming.NamespaceAndName
 import ch.datascience.graph.scope.persistence.PersistedProperties
-import ch.datascience.graph.types.{PropertyKey, StandardPropertyKey}
+import ch.datascience.graph.types.PropertyKey
 
 import scala.concurrent.Future
 
 /**
   * Created by johann on 09/05/17.
   */
-trait RelationalPersistedProperties extends PersistedProperties[NamespaceAndName] { this: ExecutionComponent with OrchestrationComponent =>
+trait RelationalPersistedProperties extends PersistedProperties { this: ExecutionComponent with OrchestrationComponent =>
 
   /**
     * Fetches property key with specified key
@@ -35,7 +35,7 @@ trait RelationalPersistedProperties extends PersistedProperties[NamespaceAndName
     * @param key
     * @return a future containing some property key if a corresponding one is found, None otherwise
     */
-  def fetchPropertyFor(key: NamespaceAndName): Future[Option[StandardPropertyKey]] = for {
+  def fetchPropertyFor(key: NamespaceAndName): Future[Option[PropertyKey]] = for {
     opt <- orchestrator.propertyKeys.findByNamespaceAndName(key)
   } yield for {
     propertyKey <- opt
@@ -49,7 +49,7 @@ trait RelationalPersistedProperties extends PersistedProperties[NamespaceAndName
     * @param keys set of keys to retrieve
     * @return map key -> property key, will not contain unknown keys
     */
-  def fetchPropertiesFor(keys: Set[NamespaceAndName]): Future[Map[NamespaceAndName, StandardPropertyKey]] = {
+  def fetchPropertiesFor(keys: Set[NamespaceAndName]): Future[Map[PropertyKey#Key, PropertyKey]] = {
     val futurePropertyKeys = Future.traverse(keys.toIterable) { key =>
       for {
         opt <- orchestrator.propertyKeys.findByNamespaceAndName(key)
