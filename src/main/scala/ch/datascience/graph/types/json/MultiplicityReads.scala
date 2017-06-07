8231f4e4e21bac27ff79cb0c4627c3d518ecab93
Johann-Michael Thiebaut <johann-michael.thiebaut@epfl.ch>
2017-06-07 09:12:10 +0200

    Added edge labels

--- src/main/scala/ch/datascience/graph/types/json/MultiplicityReads.scala
+++ src/main/scala/ch/datascience/graph/types/json/MultiplicityReads.scala
@@ -16,22 +16,22 @@
  * limitations under the License.
  */
 
-package ch.datascience.graph.elements.json
+package ch.datascience.graph.types.json
 
-import ch.datascience.graph.Constants.Key
-import ch.datascience.graph.elements.{Property, Record}
-import play.api.libs.json.{JsPath, JsValue, Writes}
+import ch.datascience.graph.types.Multiplicity
+import play.api.libs.json._
 
 /**
-  * Created by johann on 24/05/17.
+  * Created by johann on 07/06/17.
   */
-class RecordWrites[P <: Property : Writes] extends Writes[Record { type Prop <: P }] {
+object MultiplicityReads extends Reads[Multiplicity] {
 
-  def writes(record: Record { type Prop <: P }): JsValue = (JsPath \ "properties").write[record.Properties].writes(record.properties)
-
-//  private[this] implicit lazy val mapWrites: Writes[Map[P#Key, P]] = KeyFormat.mapWrites[P](implicitly[Writes[P]])
-  private[this] implicit lazy val mapWrites: Writes[Map[P#Key, P]] = new Writes[Map[Key, P]] {
-    def writes(m: Map[Key, P]): JsValue = implicitly[Writes[Iterable[P]]].writes(m.values)
+  def reads(json: JsValue): JsResult[Multiplicity] = json.validate[String] flatMap { str =>
+    try {
+      JsSuccess(Multiplicity(str))
+    } catch {
+      case e: IllegalArgumentException => JsError(e.getMessage)
+    }
   }
 
 }
--- src/main/scala/ch/datascience/graph/types/json/package.scala
+++ src/main/scala/ch/datascience/graph/types/json/package.scala
@@ -36,4 +36,6 @@ package object json {
 
   implicit lazy val CardinalityFormat: Format[Cardinality] = Format(CardinalityReads, CardinalityWrites)
 
+  implicit lazy val MultiplicityFormat: Format[Multiplicity] = Format(MultiplicityReads, MultiplicityWrites)
+
 }
