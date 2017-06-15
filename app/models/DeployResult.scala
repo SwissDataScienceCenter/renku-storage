6204db4e1ae780f430a7f4668f755a3781d3ac68
Julien Eberle <julien.eberle@epfl.ch>
2017-06-15 15:28:25 +0200

    adding list

--- app/models/DeployResult.scala
+++ app/models/DeployResult.scala
@@ -21,5 +21,5 @@ package models
 /**
   * Created by jeberle on 09.06.17.
   */
-case class DeployResult(deployId: String, success: Boolean, externalUrl: Option[String])
+case class DeployResult(deployId: String, success: Boolean, externalUrl: Option[String], message: String)
 
