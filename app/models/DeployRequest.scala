d96fef8a1959e61f73bf6b1d53ca1b57ef390dcb
Julien Eberle <julien.eberle@epfl.ch>
2017-06-20 17:22:38 +0200

    cleanup and env var

--- app/models/DeployRequest.scala
+++ app/models/DeployRequest.scala
@@ -20,4 +20,4 @@ package models
 /**
   * Created by jeberle on 09.06.17.
   */
-case class DeployRequest(deployId: String, dockerImage: String, networkPort: Option[Int])
+case class DeployRequest(deployId: String, dockerImage: String, networkPort: Option[Int], env: Option[Map[String, String]])
--- app/models/json/package.scala
+++ app/models/json/package.scala
@@ -18,6 +18,8 @@
 
 package models
 
+import ch.datascience.graph.elements.mutation.Mutation
+import ch.datascience.graph.elements.mutation.json.MutationFormat
 import play.api.libs.json._
 
 /**
@@ -27,5 +29,5 @@ package object json {
 
   implicit lazy val deployRequestReads: Reads[DeployRequest] = DeployRequestMappers.deployRequestReads
   implicit lazy val deployResultWrite: Writes[DeployResult] = DeployResultMappers.deployResultWrite
-
+  implicit lazy val mutationFormat: Format[Mutation] = MutationFormat
 }
