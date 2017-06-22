f4466f9ff6694b7f2e97870b23ae26e3d0141fc6
Johann-Michael Thiebaut <johann-michael.thiebaut@epfl.ch>
2017-06-22 13:46:18 +0200

    added EdgeLabel for type system & fixed dataType -> data_type

--- src/main/scala/ch/datascience/graph/types/json/EdgeLabelFormat.scala
+++ src/main/scala/ch/datascience/graph/types/json/EdgeLabelFormat.scala
@@ -18,28 +18,26 @@
 
 package ch.datascience.graph.types.json
 
-import ch.datascience.graph.types.{DataType, PropertyKey, Cardinality}
-import play.api.libs.json._
+import ch.datascience.graph.types.{Multiplicity, EdgeLabel}
 import play.api.libs.functional.syntax._
+import play.api.libs.json._
 
 /**
   * Created by johann on 19/06/17.
   */
-object PropertyKeyFormat extends Format[PropertyKey] {
+object EdgeLabelFormat extends Format[EdgeLabel] {
 
-  def writes(propertyKey: PropertyKey): JsValue = writer.writes(propertyKey)
+  def writes(edgeLabel: EdgeLabel): JsValue = writer.writes(edgeLabel)
 
-  def reads(json: JsValue): JsResult[PropertyKey] = reader.reads(json)
+  def reads(json: JsValue): JsResult[EdgeLabel] = reader.reads(json)
 
-  private[this] def writer: Writes[PropertyKey] = (
-    (JsPath \ "key").write[PropertyKey#Key](propKeyFormat) and
-      (JsPath \ "data_type").write[DataType](DataTypeFormat) and
-      (JsPath \ "cardinality").write[Cardinality](CardinalityFormat)
-    )(unlift(PropertyKey.unapply))
+  private[this] def writer: Writes[EdgeLabel] = (
+    (JsPath \ "key").write[EdgeLabel#Key](propKeyFormat) and
+      (JsPath \ "multiplicity").write[Multiplicity](MultiplicityFormat)
+    )(unlift(EdgeLabel.unapply))
 
-  private[this] def reader: Reads[PropertyKey] = (
-    (JsPath \ "key").read[PropertyKey#Key](propKeyFormat) and
-      (JsPath \ "data_type").read[DataType](DataTypeFormat) and
-      (JsPath \ "cardinality").read[Cardinality](CardinalityFormat)
-    )(PropertyKey.apply _)
+  private[this] def reader: Reads[EdgeLabel] = (
+    (JsPath \ "key").read[EdgeLabel#Key](propKeyFormat) and
+      (JsPath \ "multiplicity").read[Multiplicity](MultiplicityFormat)
+    )(EdgeLabel.apply _)
 }
