f1f9c71feafc22b9f8cea9d4cd4d1a9fd5bcb817
Julien Eberle <julien.eberle@epfl.ch>
2017-06-23 17:15:43 +0200

    multiports

--- app/models/DeployRequest.scala
+++ app/models/DeployRequest.scala
@@ -20,4 +20,4 @@ package models
 /**
   * Created by jeberle on 09.06.17.
   */
-case class DeployRequest(deployId: String, dockerImage: String, networkPort: Option[Int], env: Option[Map[String, String]])
+case class DeployRequest(deployId: String, dockerImage: String, networkPort: Option[List[Int]], env: Option[Map[String, String]])
--- app/models/DeployResult.scala
+++ app/models/DeployResult.scala
@@ -21,5 +21,5 @@ package models
 /**
   * Created by jeberle on 09.06.17.
   */
-case class DeployResult(deployId: String, success: Boolean, externalUrl: Option[String], message: String)
+case class DeployResult(deployId: String, success: Boolean, portForwarding: Map[String, String], images: List[String], message: String)
 
