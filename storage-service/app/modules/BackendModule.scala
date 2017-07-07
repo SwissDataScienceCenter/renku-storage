b71e340a3fa94f035d06cbfe28a69880570fc2bc
Johann-Michael Thiebaut <johann-michael.thiebaut@epfl.ch>
2017-07-07 16:56:37 +0200

    first draft for local fs and shaded notimplemented routes

--- storage-service/app/modules/BackendModule.scala
+++ storage-service/app/modules/BackendModule.scala
@@ -1,8 +1,8 @@
 package modules
 
-import controllers.storageBackends.{Backend, SwiftBackend}
-import play.api.{Configuration, Environment}
+import controllers.storageBackends.{Backend, LocalFSBackend, SwiftBackend}
 import play.api.inject.{Binding, Module}
+import play.api.{Configuration, Environment}
 
 /**
   * Created by johann on 07/07/17.
@@ -11,7 +11,8 @@ class BackendModule extends Module {
 
   def bindings(environment: Environment, configuration: Configuration): Seq[Binding[Backend]] = {
     Seq(
-      bind(classOf[Backend]).qualifiedWith("swift").to(classOf[SwiftBackend])
+      bind(classOf[Backend]).qualifiedWith("swift").to(classOf[SwiftBackend]),
+      bind(classOf[Backend]).qualifiedWith("local").to(classOf[LocalFSBackend])
     )
   }
 
