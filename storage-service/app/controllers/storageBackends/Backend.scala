bb329ac6c100f4647bf3cad30f666a8252efa565
Johann-Michael Thiebaut <johann-michael.thiebaut@epfl.ch>
2017-07-28 10:51:28 +0200

    storage-service: reformatted

--- storage-service/app/controllers/storageBackends/Backend.scala
+++ storage-service/app/controllers/storageBackends/Backend.scala
@@ -21,14 +21,14 @@ package controllers.storageBackends
 import akka.stream.scaladsl.Source
 import akka.util.ByteString
 import play.api.libs.streams.Accumulator
-import play.api.mvc.{RequestHeader, Result}
+import play.api.mvc.{ RequestHeader, Result }
 
 /**
-  * Created by jeberle on 07.07.17.
-  */
+ * Created by jeberle on 07.07.17.
+ */
 trait Backend {
-  def read(request: RequestHeader, bucket: String, name: String): Option[Source[ByteString, _]]
-  def write(request: RequestHeader, bucket: String, name: String):  Accumulator[ByteString, Result]
-  def createBucket(request: RequestHeader, bucket: String): String
+  def read( request: RequestHeader, bucket: String, name: String ): Option[Source[ByteString, _]]
+  def write( request: RequestHeader, bucket: String, name: String ): Accumulator[ByteString, Result]
+  def createBucket( request: RequestHeader, bucket: String ): String
 
 }
