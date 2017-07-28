ee10fbb40f25f5a6d1a0061b13bab0251d20feeb
Johann-Michael Thiebaut <johann-michael.thiebaut@epfl.ch>
2017-07-28 10:51:28 +0200

    storage-service: reformatted

--- storage-service/app/modules/BackendModule.scala
+++ storage-service/app/modules/BackendModule.scala
@@ -1,19 +1,19 @@
 package modules
 
-import controllers.storageBackends.{Backend, LocalFSBackend, SwiftBackend}
-import play.api.inject.{Binding, Module}
-import play.api.{Configuration, Environment}
+import controllers.storageBackends.{ Backend, LocalFSBackend, SwiftBackend }
+import play.api.inject.{ Binding, Module }
+import play.api.{ Configuration, Environment }
 
 /**
-  * Created by johann on 07/07/17.
-  */
+ * Created by johann on 07/07/17.
+ */
 class BackendModule extends Module {
 
-  def bindings(environment: Environment, configuration: Configuration): Seq[Binding[Backend]] = {
+  def bindings( environment: Environment, configuration: Configuration ): Seq[Binding[Backend]] = {
     for {
-      (name, clazz) <- availableBindings.toSeq
-      if configuration.getBoolean(s"storage.backend.$name.enabled").getOrElse(false)
-    } yield bind(classOf[Backend]).qualifiedWith(name).to(clazz)
+      ( name, clazz ) <- availableBindings.toSeq
+      if configuration.getBoolean( s"storage.backend.$name.enabled" ).getOrElse( false )
+    } yield bind( classOf[Backend] ).qualifiedWith( name ).to( clazz )
   }
 
   protected def availableBindings: Map[String, Class[_ <: Backend]] = Map(
