20ae4f69904e7318162dd02ca50bb25995158eb4
Johann-Michael Thiebaut <johann-michael.thiebaut@epfl.ch>
2017-07-28 10:51:28 +0200

    storage-service: reformatted

--- storage-service/app/controllers/RequestHelper.scala
+++ storage-service/app/controllers/RequestHelper.scala
@@ -11,42 +11,43 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
 /**
-  * Created by jeberle on 04.07.17.
-  */
+ * Created by jeberle on 04.07.17.
+ */
 trait RequestHelper { this: Controller =>
 
-  def getVertices(id: PersistedVertex#Id)(implicit graphExecutionContext: GraphExecutionContext,
-                  graphTraversalSource: GraphTraversalSource, vertexReader: VertexReader
-                 ): Future[Map[String, PersistedVertex]] = {
+  def getVertices( id: PersistedVertex#Id )( implicit
+      graphExecutionContext: GraphExecutionContext,
+                                             graphTraversalSource: GraphTraversalSource, vertexReader: VertexReader
+  ): Future[Map[String, PersistedVertex]] = {
     val g = graphTraversalSource
-    val t = g.V(Long.box(id)).as("data").out("resource:stored_in").as("bucket").select[Vertex]("data", "bucket")
+    val t = g.V( Long.box( id ) ).as( "data" ).out( "resource:stored_in" ).as( "bucket" ).select[Vertex]( "data", "bucket" )
 
-    Future.sequence(graphExecutionContext.execute {
+    Future.sequence( graphExecutionContext.execute {
       import collection.JavaConverters._
-      if (t.hasNext) {
+      if ( t.hasNext ) {
         val jmap: Map[String, Vertex] = t.next().asScala.toMap
         for {
-          (key, value) <- jmap
+          ( key, value ) <- jmap
         } yield for {
-          vertex <- vertexReader.read(value)
+          vertex <- vertexReader.read( value )
         } yield key -> vertex
       }
       else
         Seq.empty
-    }).map(_.toMap)
+    } ).map( _.toMap )
   }
 
-
-  def getVertex(id: PersistedVertex#Id)(implicit graphExecutionContext: GraphExecutionContext,
-                graphTraversalSource: GraphTraversalSource, vertexReader: VertexReader
-                ): Future[Option[PersistedVertex]] = {
+  def getVertex( id: PersistedVertex#Id )( implicit
+      graphExecutionContext: GraphExecutionContext,
+                                           graphTraversalSource: GraphTraversalSource, vertexReader: VertexReader
+  ): Future[Option[PersistedVertex]] = {
     val g = graphTraversalSource
-    val t = g.V(Long.box(id))
+    val t = g.V( Long.box( id ) )
 
     val future: Future[Option[PersistedVertex]] = graphExecutionContext.execute {
-      if (t.hasNext) {
+      if ( t.hasNext ) {
         val vertex = t.next()
-        vertexReader.read(vertex).map(Some.apply)
+        vertexReader.read( vertex ).map( Some.apply )
       }
       else
         Future.successful( None )
