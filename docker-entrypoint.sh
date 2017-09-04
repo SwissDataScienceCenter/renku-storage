749653e01afe9a15bd912529e3d0339a635f8859
Johann-Michael Thiebaut <johann-michael.thiebaut@epfl.ch>
2017-09-04 08:18:44 +0200

    Do not use host user trick if mounted /data belongs to root

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,14 +11,16 @@ if [[ "${STORAGE_BACKEND_LOCAL_ENABLED,,}" = "true" ]]; then
         file="/data/$file"
         uid=$(stat -c "%u" $file)
         gid=$(stat -c "%g" $file)
-        addgroup -g $gid daemon2
-        adduser -h /sbin -g daemon2 -s /sbin/nologin -G daemon2 -D -u $uid daemon2
-        addgroup daemon2 daemon
-        addgroup daemon2 bin
-        addgroup daemon2 adm
-        user=daemon2
-        chown -R "$uid:$gid" .
-        echo "running as $user, uid=$uid, gid=$gid"
+        if [[ $uid -ne 0 ]]; then # .gitkeep is 0:0 os osxfs mounts, do nothing
+            addgroup -g $gid daemon2
+            adduser -h /sbin -g daemon2 -s /sbin/nologin -G daemon2 -D -u $uid daemon2
+            addgroup daemon2 daemon
+            addgroup daemon2 bin
+            addgroup daemon2 adm
+            user=daemon2
+            chown -R "$uid:$gid" .
+            echo "running as $user, uid=$uid, gid=$gid"
+        fi
     fi
 fi
 
